b3290c80bc96abe09052e77df3f0f4b6c5445e7c
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-07-15 15:51:33 +0800

    feat: dynamic routes

---
+++ apps/docs/routes.py
@@ -0,0 +1,13 @@
+from virgo.core.routing import routes
+from virgo.core.response import Response, redirect
+from virgo.core.template import render
+
+
+def docs_page(request, page):
+    page_name = request.path.split("/")[-1] or "overview"
+    context = {
+        "current_path": request.path,
+        "page": page_name
+    }
+    return render("documentation.html", {"context":context}, app="docs",)
+routes["/docs/<page>"] = docs_page
